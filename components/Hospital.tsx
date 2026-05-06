a00b47397887b1e555a27974b20e8cf7c6241339
rasikhaaliya <rasikhaaliya@gmail.com>
2026-05-06 09:16:56 +0700

    benerin

--- components/Hospital.tsx
+++ components/Hospital.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { motion } from 'motion/react';
+import { motion } from "framer-motion";
 import { Search, ExternalLink, Building2, MapPin } from 'lucide-react';
 
 export default function Hospital() {
